914781d7906175d68421e756cb4c01c65b7c2ec0
vinka123 <ktnvctr123@gmail.com>
2020-04-07 16:26:36 +0300

    Add introduction paragraph

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+var add=function(num1, num2) {
+return num1+num2;
+}
+var num1=parseInt(prompt('Enter a number:'));
+var num2=parseInt(prompt('Another number:'));
+var result=add(num1, num2);
+alert(result);
+//function(subtract)
+var subtract=function(numb1, numb2) {
+return numb1-numb2;
+}
+var numb1=parseInt(prompt('Enter a number:'));
+var numb2=parseInt(prompt('Another number:'));
+var result=subtract(numb1, numb2);
+alert(result);
+//function(multiply)
+var multiply=function(numA, numB) {
+return numA*numB;
+}
+var numA=parseInt(prompt('Enter a number:'));
+var numB=parseInt(prompt('Another number:'));
+var result=multiply(numA, numB);
+alert(result);
+//function(divide)
+var divide=function(numbA, numbB) {
+return numbA/numbB;
+}
+var numbA=parseInt(prompt('Enter a number:'));
+var numbB=parseInt(prompt('Another number:'));
+var result=divide(numbA, numbB);
+alert(result);
+
+
